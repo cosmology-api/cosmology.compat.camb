c158bd057ad70c594b7c954d244a2199c7399153
Nathaniel Starkman <nstarman@users.noreply.github.com>
2023-01-25 10:15:56 -0500

    Use OpenAstronomy workflow (#4)
    * use OpenAstronomy workflow
    * fix import of constants
    * Add speed of light to constants
    * TMP disable mac test
    
    Signed-off-by: nstarman <nstarkman@protonmail.com>

--- src/cosmology/compat/camb/constants.py
+++ src/cosmology/compat/camb/constants.py
@@ -7,7 +7,11 @@ From the :mod:`cosmology.api`, the list of required constants is:
 - G: Gravitational constant G in pc km2 s-2 Msol-1.
 """
 
+from camb.constants import c as c_ms
+
 __all__ = ["G"]
+
+c = c_ms / 1e3  # [km s-1]
 
 
 G = 4.30091727003628e-3  # [pc km2 s-2 Msol-1]  CODATA 2018 value
