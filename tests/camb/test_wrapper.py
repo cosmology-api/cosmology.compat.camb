89726e8faf9607c88b994c3beca628604a1a2679
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-04-07 12:53:57 +0100

    Add `H_over_H0` (#47)
    Signed-off-by: Patrick J. Roddy <patrickjamesroddy@gmail.com>
    Co-authored-by: Nicolas Tessore <n.tessore@ucl.ac.uk>

--- tests/camb/test_wrapper.py
+++ tests/camb/test_wrapper.py
@@ -24,7 +24,8 @@ def compare():
         Ob0=0.05,
         Ode0=0.8,
         Tcmb0=2.73,
-        m_nu=[0.0, 0.0, 0.05],
+        Neff=0.0,
+        m_nu=0.0,
     )
 
 
@@ -34,11 +35,13 @@ def cosmo(compare):
         H0=compare.H0.value,
         omch2=(compare.Om0 - compare.Ob0) * compare.h**2,
         ombh2=compare.Ob0 * compare.h**2,
-        omnuh2=compare.Onu0 * compare.h**2,
+        omnuh2=0.0,
         omk=compare.Ok0,
         TCMB=compare.Tcmb0.value,
-        num_nu_massless=compare.Neff - 1,
-        num_nu_massive=1,
+        num_nu_massless=0.0,
+        num_nu_massive=0,
+        mnu=0.0,
+        nnu=0.0,
     )
     results = camb.get_background(pars)
     return cosmology.compat.camb.Cosmology(pars, results)
@@ -55,7 +58,7 @@ def test_H0(cosmo, compare):
 def test_Omega_m0(cosmo, compare):
     np.testing.assert_allclose(
         cosmo.Omega_m0,
-        compare.Om0 + compare.Onu0,
+        compare.Om0,
         rtol=1e-10,
     )
 
@@ -64,7 +67,7 @@ def test_Omega_de0(cosmo, compare):
     np.testing.assert_allclose(
         cosmo.Omega_de0,
         compare.Ode0,
-        rtol=1e-3,
+        rtol=1e-10,
     )
 
 
@@ -84,7 +87,7 @@ def test_H(z, cosmo, compare):
     np.testing.assert_allclose(
         cosmo.H(z),
         compare.H(z).value,
-        rtol=1e-3,
+        rtol=1e-12,
     )
 
 
@@ -150,4 +153,12 @@ def test_angular_diameter_distance(z, cosmo, compare):
         cosmo.angular_diameter_distance(z1, z2),
         compare.angular_diameter_distance_z1z2(z1, z2).value,
         rtol=1e-3,
+    )
+
+
+def test_H_over_H0(z, cosmo, compare):
+    np.testing.assert_allclose(
+        cosmo.H_over_H0(z),
+        compare.efunc(z),
+        rtol=1e-12,
     )
